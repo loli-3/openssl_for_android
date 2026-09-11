d33e29cc076028ff1706e979a33801500ceef880
loli-3 <171003488+loli-3@users.noreply.github.com>
2026-09-11 20:09:35 +0800

    Update openssl_build_new.sh
    remove useless libraries for my use case.

--- openssl_build_new.sh
+++ openssl_build_new.sh
@@ -31,7 +31,7 @@ ANDROID_NDK_VERSION=$4
 ANDROID_NDK_PATH=${WORK_PATH}/android-ndk-${ANDROID_NDK_VERSION}
 OPENSSL_PATH=${WORK_PATH}/openssl-${OPENSSL_VERSION}
 OUTPUT_PATH=${WORK_PATH}/openssl_${OPENSSL_VERSION}_${ANDROID_TARGET_ABI}
-OPENSSL_OPTIONS="no-apps no-asm no-docs no-engine no-gost no-legacy no-shared no-ssl no-tests no-zlib"
+OPENSSL_OPTIONS="no-aes no-des no-idea no-camellia no-seed no-bf no-cast no-rc2 no-rc4 no-rc5 no-md2 no-md4 no-md5 no-sha no-ripemd no-mdc2 no-dsa no-dh no-ec no-ecdsa no-ecdh no-ssl3 no-tls1 no-tls1_1 no-tls1_2 no-dtls no-comp no-sock no-engine no-hw no-krb5 no-cms no-jpake no-capieng no-ocsp no-psk no-srp"
 
 if [ "$(uname -s)" == "Darwin" ]; then
     echo "Build on macOS..."
